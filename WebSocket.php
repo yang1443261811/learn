61ca2ac49721c688741c0e6f3f28a93e2989297f
yang1443261811 <1443261811@qq.com>
2019-12-20 18:19:30 +0800

    1

--- WebSocket.php
+++ WebSocket.php
@@ -62,7 +62,7 @@ class WebSocket
     {
         $this->port = $port;
         $this->host = $host;
-        static::$globalEvent = $this->getEventPollClass();
+        static::$globalEvent = $this->getEventPollClass('event');
     }
 
     public function run()
