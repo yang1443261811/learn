ec060414c4f7c30fb2b65576ebdbf6bc7d71d195
yang1443261811 <1443261811@qq.com>
2019-12-20 17:32:07 +0800

    1

--- Timer.php
+++ Timer.php
@@ -1,30 +1,37 @@
 <?php
 
-
+/**
+ * Timer.
+ *
+ * example:
+ * Workerman\Lib\Timer::add($time_interval, callback, array($arg1, $arg2..));
+ */
 class Timer
 {
     /**
-     * 任务列表
+     * Tasks that based on ALARM signal.
+     * [
+     *   run_time => [[$func, $args, $persistent, time_interval],[$func, $args, $persistent, time_interval],..]],
+     *   run_time => [[$func, $args, $persistent, time_interval],[$func, $args, $persistent, time_interval],..]],
+     *   ..
+     * ]
      *
      * @var array
      */
-    protected static $tasks = [];
+    protected static $_tasks = array();
 
     /**
-     * 初始化
+     * Init.
      *
      * @return void
      */
     public static function init()
     {
-        //安装信号处理方法
-        if (function_exists('pcntl_signal')) {
-            pcntl_signal(SIGALRM, array('Timer', 'signalHandle'), false);
-        }
+        pcntl_signal(SIGALRM, array('\Workerman\Lib\Timer', 'signalHandle'), false);
     }
 
     /**
-     * 接收到信号执行处理
+     * ALARM signal handler.
      *
      * @return void
      */
@@ -35,73 +42,93 @@ class Timer
     }
 
     /**
-     * 对任务列表进行循环,执行满足条件的任务
+     * Add a timer.
+     *
+     * @param int $time_interval
+     * @param callback $func
+     * @param mixed $args
+     * @param bool $persistent
+     * @return int/false
+     */
+    public static function add($time_interval, $func, $args = array(), $persistent = true)
+    {
+        if ($time_interval <= 0) {
+            echo new Exception("bad time_interval");
+            return false;
+        }
+
+        if (!is_callable($func)) {
+            echo new Exception("not callable");
+            return false;
+        }
+
+        if (empty(self::$_tasks)) {
+            pcntl_alarm(1);
+        }
+
+        $time_now = time();
+        $run_time = $time_now + $time_interval;
+        if (!isset(self::$_tasks[$run_time])) {
+            self::$_tasks[$run_time] = array();
+        }
+        self::$_tasks[$run_time][] = array($func, (array)$args, $persistent, $time_interval);
+        return 1;
+    }
+
+
+    /**
+     * Tick.
      *
      * @return void
      */
     public static function tick()
     {
-        //没有任务,返回
-        if (empty(static::$tasks)) {
+        if (empty(self::$_tasks)) {
+            pcntl_alarm(0);
             return;
         }
 
-        foreach (static::$tasks as $time => $arr) {
-            $current = time();
-            //如果满足执行条件,遍历每一个任务
-            if ($current >= $time) {
-                foreach ($arr as $k => $job) {
-                    //回调函数
-                    $func = $job['func'];
-                    //回调函数参数
-                    $argv = $job['argv'];
-                    //时间间隔
-                    $interval = $job['interval'];
-                    //持久化
-                    $persist = $job['persist'];
-                    //当前时间有执行任务
-                    //调用回调函数,并传递参数
-                    call_user_func_array($func, $argv);
-                    //如果做持久化,则写入数组,等待下次唤醒
-                    if ($persist) {
-                        static::$tasks[$current + $interval][] = $job;
+        $time_now = time();
+        foreach (self::$_tasks as $run_time => $task_data) {
+            if ($time_now >= $run_time) {
+                foreach ($task_data as $index => $one_task) {
+                    $task_func = $one_task[0];
+                    $task_args = $one_task[1];
+                    $persistent = $one_task[2];
+                    $time_interval = $one_task[3];
+                    try {
+                        call_user_func_array($task_func, $task_args);
+                    } catch (\Exception $e) {
+                        echo $e;
+                    }
+                    if ($persistent) {
+                        self::add($time_interval, $task_func, $task_args);
                     }
                 }
-
-                //删除该任务
-                unset(static::$tasks[$time]);
+                unset(self::$_tasks[$run_time]);
             }
         }
     }
 
+    /**
+     * Remove a timer.
+     *
+     * @param mixed $timer_id
+     * @return bool
+     */
+    public static function del($timer_id)
+    {
+        return false;
+    }
 
     /**
-     * 添加任务
+     * Remove all timers.
      *
-     * @param $interval
-     * @param $func
-     * @param array $argv
-     * @param bool $persist
+     * @return void
      */
-    public static function add($interval, $func, $argv = array(), $persist = false)
+    public static function delAll()
     {
-        if (is_null($interval)) {
-            return;
-        }
-
-        if (empty(static::$tasks)) {
-            \pcntl_alarm(1);
-        }
-
-        $run_time = time() + $interval;
-        //写入定时任务
-        static::$tasks[$run_time][] = array(
-            'func'     => $func,
-            'argv'     => $argv,
-            'interval' => $interval,
-            'persist'  => $persist
-        );
+        self::$_tasks = array();
+        pcntl_alarm(0);
     }
 }
-
-(new Timer())->init();
---
+++ WebSocket.php
@@ -0,0 +1,176 @@
+<?php
+require_once './Utils.php';
+require_once './Event/Select.php';
+require_once './Event/EventInterface.php';
+
+
+class WebSocket
+{
+    /**
+     * 监听的地址
+     *
+     * @var string
+     */
+    private $host;
+
+    /**
+     * 监听的端口号
+     *
+     * @var int
+     */
+    private $port;
+
+    /**
+     * socket链接池
+     *
+     * @var array
+     */
+    protected $sockets = [];
+
+    /**
+     * socket 主服务
+     *
+     * @var
+     */
+    protected $master;
+
+    /**
+     * 接入的用户表
+     *
+     * @var array
+     */
+    protected $users = [];
+
+    /**
+     * 事件对象
+     *
+     * @var object
+     */
+    public static $globalEvent;
+
+    public $callbackConnect;
+
+    public $callbackMessage;
+
+    /**
+     * Server constructor.
+     * @param string $host
+     * @param int $port
+     */
+    public function __construct($host = '127.0.0.1', $port = 9999)
+    {
+        $this->port = $port;
+        $this->host = $host;
+        static::$globalEvent = $this->getEventPollClass();
+    }
+
+    public function run()
+    {
+        //创建socket
+        $this->master = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        //返回bool.套接字resource,协议级别,可用的socket选项,值。
+        socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1);
+
+        //将socket绑定到指定的IP:port上
+        socket_bind($this->master, $this->host, $this->port);
+
+        //开始监听
+        socket_listen($this->master);
+
+        $this->sockets[0] = ['resource' => $this->master];
+
+        //将服务器设置为非阻塞
+        socket_set_nonblock($this->master);
+
+        static::$globalEvent->add([$this, 'connect'], array('connect'), $this->master, EventInterface::EVENT_TYPE_READ);
+
+        static::$globalEvent->loop();
+    }
+
+    public function getEventPollClass($type = null)
+    {
+        if ($type == 'event') {
+            $instance = new Event();
+        } else {
+            $instance = new Select();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * 将socket加入链接池
+     *
+     * @param object $socket
+     */
+    public function connect($socket)
+    {
+        //接收一个链接
+        if (($connect = socket_accept($socket)) == false) {
+            return;
+        }
+
+        $this->sockets[(int)$connect] = [
+            'handshake' => false,
+            'resource'  => $connect,
+        ];
+
+        static::$globalEvent->add([$this, 'reader'], array('reader'), $connect, EventInterface::EVENT_TYPE_READ);
+
+        if (is_callable($this->callbackConnect)) {
+            call_user_func($this->callbackConnect, $socket);
+        }
+    }
+
+    public function reader($connect)
+    {
+        $buffer = '';
+        socket_recv($connect, $buffer, 2048, 0);
+        $id = (int)$connect;
+        if ($this->sockets[$id]['handshake']) {
+            $data = Utils::decode($buffer);
+            //执行事件回调
+            if (is_callable($this->callbackMessage)) {
+                call_user_func($this->callbackMessage, $data);
+            }
+        } else {
+            $this->handshake($connect, $buffer);
+            $this->sockets[$id]['handshake'] = true;
+        }
+    }
+
+    /**
+     * 回应握手
+     *
+     * @param object $socket
+     * @param string $buffer
+     * @return bool
+     */
+    protected function handshake($socket, $buffer)
+    {
+        //对接收到的buffer处理,并回馈握手！！
+        $buf = substr($buffer, strpos($buffer, 'Sec-WebSocket-Key:') + 18);
+        $key = trim(substr($buf, 0, strpos($buf, "\r\n")));
+        $hash = base64_encode(sha1($key . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", true));
+        $response = "HTTP/1.1 101 Switching Protocols\r\n";
+        $response .= "Upgrade: websocket\r\n";
+        $response .= "Sec-WebSocket-Version: 13\r\n";
+        $response .= "Connection: Upgrade\r\n";
+        $response .= "Sec-WebSocket-Accept: " . $hash . "\r\n\r\n";
+        //回馈握手
+        socket_write($socket, $response, strlen($response));
+        //向客户端发送握手成功消息
+        $msg = Utils::encode(json_encode([
+            'content' => 'done',
+            'type'    => 'handshake',
+        ]));
+        socket_write($socket, $msg, strlen($msg));
+
+        return true;
+    }
+
+}
+
+$webSocket = new WebSocket();
+$webSocket->run();
